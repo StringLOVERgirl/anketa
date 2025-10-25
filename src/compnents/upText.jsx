72fb462e70900d0265069ecb4819fc76f0e6286e
StringLOVERgirl <marchenkostar@gmail.com>
2025-10-25 08:19:09 +0300

    symbanimation

--- src/compnents/upText.jsx
+++ src/compnents/upText.jsx
@@ -35,6 +35,29 @@ export function Uptext () {
         })
     }, [])
 
+
+    let words = 'Thanks\u00A0for\u00A0visiting.\u00A0Have\u00A0a\u00A0nice\u00A0UX'.split('\u00A0')
+    let letters = 'Thanks\u00A0for\u00A0visiting.\u00A0Have\u00A0a\u00A0nice\u00A0UX'.split('')
+    const numbers = useRef(new Set)
+    console.log(letters)
+    let globalindex = -1
+    useEffect(()=>{
+        for (let i = 0; i < 7; i++){
+            let index = getRandomInRange(0, letters.length-1)
+            
+            numbers.current.add(index)
+        }
+        console.log(numbers.current)
+    },[])
+
+    function getRandomInRange(min, max) {
+        let index
+        do {
+            index = Math.floor(Math.random() * (max - min) + min);
+        } while (numbers.current.has(index))
+        return index
+    }
+
     return(
 
     <div className={`upTextCont ${videoState.h1ContOpacity}`}>
@@ -51,7 +74,21 @@ export function Uptext () {
             <div className={`leftTextTop ${text}`} ref={leftTextTop}>
                 {/* <div className="leftup topText">Thank you for visiting</div> */}
                 {/* <div className="leftup bottomText">Our best solutions for your personal brand</div> */}
-                <div className="leftup topText">Thanks for visiting. Have a nice UX</div>
+                <div className="leftup topText" style={{display: 'flex'}}>
+                   {words.map((ew,iw)=> {     
+                    let currentindex = globalindex
+                   globalindex+=1
+                    return  <div style={{display: 'flex'}}>
+                    {ew.split('').map((e,i)=>{
+                        // let currentindex = globalindex
+                        globalindex+=1
+                        console.log(currentindex, letters.length-1)
+                       return numbers.current.has(globalindex) && e != '.' && e.trim() != '' ? <div aria-labe={e} style={{'--delayAS': globalindex*0.5+'s'}} className='ux'></div>:<div>{e}</div>}
+                    )}
+                    {iw != words.length-1 ? '\u00A0' : ''}
+                   </div>  }         
+                   )}
+                </div>
                 {/* <div className="leftup topText">Спасибо, что решила (или решил) заглянуть </div> */}
                 {/* <div className="leftup bottomText">Здесь немного из мира моих интересов</div> */}
                 <div className="leftup bottomText">Here're some things about me.</div>
