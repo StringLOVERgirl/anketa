3a045cc4176275adca5d712b391209f9280578f9
StringLOVERgirl <marchenkostar@gmail.com>
2025-10-26 15:08:06 +0300

    castles

--- src/compnents/player.jsx
+++ src/compnents/player.jsx
@@ -4,6 +4,7 @@ import xxx from '/audio/XXXTENTACION-whoa-(mind-in-awe).mp3'
 // import chlorella from '/audio/BONES-Chlorella.mp3'
 // import xxx2 from '/audio/XXXTENTACION-AI-Selfless.mp3'
 import tracy from '/audio/Lil_Tracy_-_Tight_Rope.mp3'
+import castles from '/audio/castles.mp3'
 import peep3 from '/audio/Y2meta_app_Lil_Peep_x_Lil_Tracy_Blowing_Kisses_In_The_Wind_miro.mp3'
 // import chikoi from '/audio/Chikoi_The_Maid_-_Reflection_(SkySound.cc).mp3'
 // import burgos from '/audio/White-Girls-in-the-Back-of-the-Bus.mp3'
@@ -13,7 +14,7 @@ import brennan from '/audio/brennan.mp3'
 // import twoh from '/audio/2high.mp3'
 import kyoto from '/audio/kyoto.mp3'
 import dclxvi from '/audio/DCLXVI_-_DISSECTION_76038142.mp3'
-// import lilpeep from '/audio/Y2meta_app_LIL_PEEP_&_BOY_FROOT_THE_DAY_I_FINALLY_DO_IT_НА_РУССКОМ.mp3'
+import lilpeep from '/audio/Y2meta_app_LIL_PEEP_&_BOY_FROOT_THE_DAY_I_FINALLY_DO_IT_НА_РУССКОМ.mp3'
 import peep2 from '/audio/lil_peep_300_feet_below_pineapple_soda_without_feature_extended.mp3'
 
 
@@ -25,7 +26,8 @@ export const player = {
     brennan: ['brennan savage - i could paint...', 'brennan', brennan],
     // xxx2: ['XXXTENTACION - selfless (AI)', 'xxxost', xxx2],
     peep3: ['Lil Peep x Lil Tracy - Blowing Kisses', 'peep', peep3],
-    // peep: ['Lil Peep - The Day I Finally Do It', 'peep', lilpeep],
+    peep: ['Lil Peep - The Day I Finally Do It', 'peep', lilpeep],
+    castles: ['Lil Peep & Lil Tracy - Castles', 'castles', castles],
     // peep2: ['LiL Peep - 300 feet below', 'peep', peep2],
     // burgos: ['White Girls in the Back of the Bus', 'burgos', burgos],
     peep2: ['LiL Peep - 300 feet below', 'peep', peep2],
